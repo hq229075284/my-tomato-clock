cf1a0b9e4542281937d00a9abe9c25a3c4109b71
hq229075284 <229075284@qq.com>
2017-03-30 02:14:23 +0800

    初步完成electron+vue+webpack框架搭建

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import "./index.less"
+import "./index.scss"
+// require("./index.scss")
+
+document.querySelector("#root").innerHTML = "hqs"
+console.log({ ...{ x: 1 } })
+// document.querySelector("#root").innerHTML = JSON.stringify({ ...{ x: 1 } })
---
+++ webpack.config.js
@@ -0,0 +1,86 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+module.exports = {
+  entry: {
+    index: "./src/index.js"
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: "[name].js",
+    //  发布路径一般把path作为服务区的根路径
+    "publicPath": "http://localhost:2000/"
+  },
+  module: {
+    rules: [
+      {
+        use: "babel-loader",
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/
+      },
+      {
+        test: /\.less$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            // options: { modules: true },
+            loader: "css-loader"
+          },
+          {
+            loader: "less-loader",
+            // options: { sourceMap: true }
+          },
+        ],
+        exclude: /(node_modules|bower_components)/
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            // options: { modules: true },
+            loader: "css-loader"
+          },
+          {
+            loader: "sass-loader",
+            // options: { sourceMap: true }
+          },
+        ],
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {}
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify('development')
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+    // new ExtractTextPlugin('index.css'),
+    new webpack.NoEmitOnErrorsPlugin(),
+    // new HtmlWebpackPlugin({
+    //   title: 'my-vue-electron',
+    //   template: path.resolve(__dirname, 'index.html'),
+    //   inject: 'body'
+    // }),
+  ],
+  devtool: "source-map",
+  target: 'electron-renderer',
+  devServer: {
+    port: 2000,
+    hot: true,
+    inline: true,
+    historyApiFallback: true,
+    // contentBase: path.resolve(__dirname, 'dist'),
+    // contentBase: path.join(__dirname, 'dist'),
+    // publicPath: "http://localhost:2000/dist/"
+  },
+}
